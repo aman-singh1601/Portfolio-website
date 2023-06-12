17af71e761512bc2812fdecdddde68d21439ef78
Aman <aman16singh2001@gmail.com>
2023-06-12 18:28:07 +0530

    designing the Home page

--- src/components/Layouts/index.js
+++ src/components/Layouts/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './index.scss'
 import SideBar from '../SideBar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-  <SideBar/>
+  <div className='App'>
+    <SideBar/>
+    <div className='page'>
+    <span className='tags top-tags'> &lt;body&gt; </span>
+    
+    <Outlet/>
+
+    <span className='tags botton-tags'> 
+    &lt;/body&gt;
+    <br/>
+    <span className='bottom-tag-html'> &lt;/html&gt;</span>
+    </span>
+    </div>
+  </div>
   )}
 
 export default Layout
